01e33a7852a8d59c0deba229c9041e1d5b085525
Alex <alex@parse.cn>
2024-09-29 16:25:53 +0800

    Create Channel model and migration file

---
+++ database/migrations/0003_01_07_000001_create_channels_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Entryshop\Shop\Base\ShopMigration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends ShopMigration {
+
+    public function up(): void
+    {
+        Schema::create($this->table('channels'), function (Blueprint $table) {
+            $table->id();
+            $table->string('reference')->nullable()->index();
+            $table->string('name')->nullable()->index();
+            $table->string('status')->nullable()->index();
+            $table->boolean('is_default')->nullable()->index();
+            $table->mediumText('data')->nullable();
+            $table->timestamps();
+        });
+    }
+};
---
+++ src/Models/Channel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Entryshop\Shop\Models;
+
+use Entryshop\Shop\Base\ShopModel;
+
+class Channel extends ShopModel
+{
+    protected $guarded = [];
+}
