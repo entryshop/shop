3f2045ebfe25c9f2c9d1009687fe011410df0178
Alex <alex@parse.cn>
2024-09-29 16:29:38 +0800

    Add HasDefaultRecord and HasReference traits to Channel model

--- src/Models/Channel.php
+++ src/Models/Channel.php
@@ -3,9 +3,13 @@
 namespace Entryshop\Shop\Models;
 
 use Entryshop\Shop\Base\ShopModel;
+use Entryshop\Utils\Models\Traits\HasDefaultRecord;
+use Entryshop\Utils\Models\Traits\HasReference;
 use Entryshop\Utils\Models\Traits\VirtualColumn;
 
 class Channel extends ShopModel
 {
     use VirtualColumn;
+    use HasDefaultRecord;
+    use HasReference;
 }
